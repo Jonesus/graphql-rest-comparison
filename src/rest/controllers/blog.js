f19e833cb4e4e84b506f912103f6d469588a39ca
Elias Seeve <elias.seeve@gmail.com>
2022-12-08 18:14:06 +0200

    added endpoints

--- src/rest/controllers/blog.js
+++ src/rest/controllers/blog.js
@@ -24,6 +24,17 @@ router.get("/:id", (req, res, next) => {
     });
 });
 
+router.get("/:id/posts", (req, res, next) => {
+  const id = [req.params.id]
+  db.all(`SELECT * FROM Post WHERE blog = ?`, [id], (err, row) => {
+      if (err) {
+        res.status(400).json({"error":err.message});
+        return;
+      }
+      res.status(200).json(row);
+    });
+});
+
 router.post("/", (req, res, next) => {
   const body = req.body;
   db.run(`INSERT INTO Blog (id, name, url) VALUES (?, ?, ?)`, [body.id, body.name, body.url], (err, row) => {
--- src/rest/controllers/post.js
+++ src/rest/controllers/post.js
@@ -24,6 +24,31 @@ router.get("/:id", (req, res, next) => {
     });
 });
 
+router.get("/:id/comments", (req, res, next) => {
+  const id = [req.params.id]
+  db.all(`SELECT * FROM Comment WHERE post = ?`, [id], (err, row) => {
+      if (err) {
+        res.status(400).json({"error":err.message});
+        return;
+      }
+      res.status(200).json(row);
+    });
+});
+
+router.post("/:id/comments", (req, res, next) => {
+  const postId = [req.params.id]
+  const body = req.body;
+  db.run(`INSERT INTO Comment (id, body, post, parent, user) VALUES (?, ?, ?, ?, ?)`, [body.id, body.body, postId, body.parent, body.user], (err, row) => {
+      if (err) {
+        res.status(400).json({"error":err.message});
+        return;
+      }
+      res.status(201).json({
+        "comment_id": body.id
+    })
+    });
+});
+
 router.post("/", (req, res, next) => {
   const body = req.body;
   db.run(`INSERT INTO Post (id, title, body, blog, author) VALUES (?, ?, ?, ?, ?)`, [body.id, body.title, body.body, body.blog, body.author], (err, row) => {
